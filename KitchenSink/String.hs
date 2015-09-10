d7527dd1e3c9d1502ab1f5d366593e17a8459caa
Mike Zuser <mikezuser@gmail.com>
2015-09-10 17:18:06 -0400

    KitchenSink.String: added multiline
    multiline is a hack for using do-notation for Strings
    required OverloadedStrings

--- KitchenSink/String.hs
+++ KitchenSink/String.hs
@@ -14,14 +14,38 @@
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -- | Miscellaneous string functions.
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving, FlexibleInstances #-}
 module KitchenSink.String
   ( chomp
+  , multiline, Lines
   ) where
+
+import Control.Applicative
+import Control.Monad.Writer
 import Data.List
+import Data.String
 
 -- | Remove trainling newlines from a string
 chomp :: String -> String
 chomp = dropWhileEnd (== '\n')
 
 -- | 'multiline' is a hack for using do-notation and OverloadedStrings to write
+-- multiline strings
+--
+-- @
+--    {-# LANGUAGE OverloadedStrings #-}
+--
+--    twolines = multiline $ do
+--      "line1"
+--      "line2"
+--    -- twolines == "line1\nline\n"
+-- @
+multiline :: Lines -> String
+multiline = execWriter . lineWriter
 
+type Lines = LineWriter ()
+newtype LineWriter a = LineWriter { lineWriter :: Writer String a }
+  deriving (Functor, Applicative, Monad)
+
+instance IsString (LineWriter a) where
+  fromString s = LineWriter $ tell (s ++ "\n") >> return undefined
